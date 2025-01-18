44cd0383a36e33a876427bc992a60ce08fe354dd
Abdul Motaleb <71353762+learnwithmotaleb@users.noreply.github.com>
2025-01-18 09:21:46 +0600

    Add files via upload

---
+++ models/users.model.js
@@ -0,0 +1,53 @@
+const { v4: uuidv4} = require("uuid");
+const users = [
+
+     {
+          id:uuidv4(),
+          username: "motaleb",
+          email:"lwmsoftltd@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "nila",
+          email:"lwmsoftltd1@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "sonia",
+          email:"lwmsoftltd@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "lipon",
+          email:"lwmsoftltd@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "safin",
+          email:"lwmsoftltd@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "alamin",
+          email:"lwmsoftltd@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "Parsi Khan",
+          email:"example@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "Tamin Khan",
+          email:"taminm@gmail.com"
+     },
+     {
+          id:uuidv4(),
+          username: "ripon",
+          email:"lwmsoftltd@gmail.com"
+     }
+
+]
+
+
+module.exports = users;
